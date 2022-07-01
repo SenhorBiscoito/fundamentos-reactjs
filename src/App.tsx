9bd6b29a9041fe4c0c6171496288a76ff031c51b
Jean Oliveira Miranda <mirandajean2009@gmail.com>
2022-06-30 22:10:54 -0300

    feat: ✨ creating comment component and adding styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { Header } from './components/Header/Header';
+import { Header } from "./components/Header/Header";
 
-import './global.css';
-import styles from './App.module.css';
-import { Post } from './components/Post/Post';
-import { Sidebar } from './components/Sidebar/Sidebar';
+import "./global.css";
+import styles from "./App.module.css";
+import { Post } from "./components/Post/Post";
+import { Sidebar } from "./components/Sidebar/Sidebar";
 
 export function App() {
   return (
@@ -17,12 +17,8 @@ export function App() {
             author="Fulano"
             content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius consectetur, nobis repellat fugiat porro laborum ad reiciendis? Voluptatum ipsum cum ipsam perferendis voluptatem labore perspiciatis aliquam dolorum velit, illo exercitationem!"
           />
-          <Post
-            author="Fulano Two"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius consectetur, nobis repellat fugiat porro laborum ad reiciendis? Voluptatum ipsum cum ipsam perferendis voluptatem labore perspiciatis aliquam dolorum velit, illo exercitationem!"
-          />
         </main>
       </div>
     </div>
-  )
+  );
 }
--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,3 +1,7 @@
+import { Comment } from "../Comment/Comment";
+
+import styles from "./Post.module.css";
+
 interface IPost {
   author: string;
   content: string;
@@ -5,9 +9,54 @@ interface IPost {
 
 export function Post(props: IPost) {
   return (
-    <div>
-      <b>{props.author}</b>
-      <p>{props.content}</p>
-    </div>
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img src="https://github.com/SenhorBiscoito.png" />
+          <div className={styles.authorInfo}>
+            <strong>SenhorBiscoito</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time
+          title="30 de Junho de 2022 às 22:00"
+          dateTime="2022-06-04T22:00:00"
+        >
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p>
+          👉 <a href="#">jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="#">#novoprojeto</a> <a href="#">#nlw</a>{" "}
+          <a href="#">#rocketseat</a>
+        </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea placeholder="Deixe um comentário aqui..." />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+
+      <div className={styles.commentLint}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
+    </article>
   );
 }
