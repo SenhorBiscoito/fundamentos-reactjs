28f46627b1f3537b99dda25ddf64ffd298fdf8be
Jean Oliveira Miranda <mirandajean2009@gmail.com>
2022-06-30 22:22:01 -0300

    feat: ✨ creating avatar component

---
+++ src/components/Avatar/Avatar.tsx
@@ -0,0 +1,15 @@
+import styles from "./Avatar.module.css";
+
+interface IAvatar {
+  src: string;
+  hasBorder?: boolean;
+}
+
+export function Avatar({ hasBorder = true, src }: IAvatar) {
+  return (
+    <img
+      className={hasBorder ? styles.avatarWithBorder : styles.avatar}
+      src={src}
+    />
+  );
+}
